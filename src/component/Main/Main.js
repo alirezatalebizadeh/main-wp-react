9dbb1186a78f409011c430be08e1ac5102f43321
AlirezaTalebizadeh <94745174+Alirezat78@users.noreply.github.com>
2023-08-16 03:14:07 +0330

    create title Scroll about brand

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { AiOutlineLine } from 'react-icons/ai'
 import './Main.css'
 import SliderBrands from './SliderBrands'
 
 export default function Main() {
     return (
         <div className='main_content'>
-
+            <h3 className='title_brand'>
+                <AiOutlineLine className='logo' />
+                <span className='green_text'>برند هایی </span>   که از افزونه مین وردپرس استفاده می کنند
+                <AiOutlineLine className='logo' /></h3>
             <SliderBrands />
         </div>
     )
